098f219e5b7cf8884566b34891f743b4c3366aaf
AlexandrLytkin <Dracca@mail.ru>
2022-09-26 20:26:32 +0300

    поправил

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,29 @@ public class Main {
         task3();
     }
 
-    public static void task3() {
+    private static void task3() {
         System.out.println("Задание 3");
         int deliDistance = 95;
         deliDistance2(deliDistance);
         System.out.println("дистанция " + deliDistance + " км, потребуется дней: "+ deliDistance2(deliDistance) + " + срок доставки");
     }
-    private static int deliDistance2(int deliDistance2) {
-        int a = deliDistance2;
-        if (a >= 0 && a <= 20)
+    private static int deliDistance2(int days) {
+        int a = days;
+        if (a >= 0 && a <= 20) {
             return 1;
-        if (a > 20 && a <= 60)
+        }
+        if (a > 20 && a <= 60) {
             return 2;
-        if (a > 60 && a <= 100)
+        }
+        if (a > 60 && a <= 100) {
             return 3;
+        }
         else {
-            System.out.print("не корректное число ");
-            return deliDistance2;
+            return days;
         }
     }
 
-    public static void checkYearProduction(int a, int b) {
+    private static void checkYearProduction(int a, int b) {
         int yearCheck = a;
         int checkOS = b;
             if (checkOS == 0) {
@@ -46,16 +48,15 @@ public class Main {
                 }
             }
         }
-    public static void task2() {
+    private static void task2() {
         System.out.println("Задание 2");
-        // int currentYear = LocalDate.now().getYear();  // можно использовать для вывода текущено года!
-        int yearProduction = 2014;
+        int clientDeviceYear = LocalDate.now().getYear();  // можно использовать для вывода текущено года!
         int clientOS = 0;
-        checkYearProduction(yearProduction, clientOS);
+        checkYearProduction(clientDeviceYear, clientOS);
 
     }
 
-    public static void checkYear(int a) {
+    private static void checkYear(int a) {
         int yearCheck = a;
             if (yearCheck % 4 == 0 && yearCheck % 100 != 0 || yearCheck % 400 == 0) {
                 System.out.println(yearCheck + " год является високосным");
@@ -63,7 +64,7 @@ public class Main {
                 System.out.println(yearCheck + " год не является високосным");
         }
     }
-    public static void task1() {
+    private static void task1() {
         System.out.println("Задание 1");
         int year = 2022;
         checkYear(year);
